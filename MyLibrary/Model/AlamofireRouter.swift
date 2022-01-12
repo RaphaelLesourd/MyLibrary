5c32c81125da3f611a3b1dda6e797cdce1d12757
Birkyboy <birkyboy@icloud.com>
2022-01-12 20:48:52 +0100

    change google books api order by newest result

--- MyLibrary/Model/AlamofireRouter.swift
+++ MyLibrary/Model/AlamofireRouter.swift
@@ -51,7 +51,7 @@ enum AlamofireRouter: URLRequestConvertible {
                     "startIndex": startIndex,
                     "maxResults": 40,
                     "filter": "paid-ebooks",
-                    "orderBy": "relevance",
+                    "orderBy": "newest",
                     "zoom": 0,
                     "img": true]
         case .sendPushMessage(payload: let payload):
