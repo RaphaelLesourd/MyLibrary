bd8680c0ac198c5c790001e13a19f2fbe1f310d4
Birkyboy <birkyboy@icloud.com>
2022-01-21 22:11:37 +0100

    add newBookControllerDelegate

--- MyLibrary/Controllers/NewBook/NewBookViewControllerDelegate.swift
+++ MyLibrary/Controllers/NewBook/NewBookViewControllerDelegate.swift
@@ -5,7 +5,7 @@
 //  Created by Birkyboy on 03/01/2022.
 //
 
-protocol NewBookDelegate: AnyObject {
+protocol NewBookViewControllerDelegate: AnyObject {
     var bookDescription: String? { get set }
     var bookComment: String? { get set }
     var bookCategories : [String] { get set }
